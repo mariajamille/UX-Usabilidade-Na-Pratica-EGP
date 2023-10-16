e7649b73fd927f6a48990e5e8ae09c29266f7213
AmandaAmbrosioUchoa <amandadanyelleuchoaambrosiocp@gmail.com>
2023-10-16 19:25:52 -0300

    mudei o menu

--- modulo01/topico03.php
+++ modulo01/topico03.php
@@ -16,36 +16,6 @@ include('layout/header.php');
     include('layout/menu.php');
     ?>
 
-    <!-- CONTEUDO INICIO -->
-    <div class="all-conteudo">
-        <section class="conteiner-principal">
-            <div class="main-content container-fluid">
-                <div class="row">
-                    <!-- Begin row -->
-                    <div class="col-md-1"></div>
-                    <div class="col-md-10 col-sm-8 container-fluid">
-
-                        <h2 class="Titulo fw-bolder">Design de experiência do usuário: usabilidade na prática</h2>
-
-
-        <!-- Botões de navegação -->
-        <div class="text-center">
-            <a href="index.php" class="btn btn-outline-success" role="button" aria-pressed="true"> <i
-                    class="fa-solid fa-chevron-left"></i> Voltar </a>
-            <a href="index.php" class="btn btn-outline-success" role="button" aria-pressed="true">Próximo <i
-                    class="fa-solid fa-chevron-right"></i></a>
-        </div>
-
-        <!-- barra de progresso embaixo -->
-        <div id="progress-bar"></div>
-    </div>
-
-    <!-- BUTTON BACK TO TOP (JAMILLE) -->
-    <div id="back_to_top">
-        <div id="btn_back">
-            <i id="icon_up" class="fa-solid fa-arrow-up"></i>
-        </div>
-    </div>
 
 <?php
 include('layout/footer.php');
