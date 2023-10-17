75f84be568b6f68118c5c93206143ae2e4be1c77
AmandaAmbrosioUchoa <amandadanyelleuchoaambrosiocp@gmail.com>
2023-10-16 21:03:36 -0300

    terminei referencias e comecei o topico 03

--- modulo01/referencias.php
+++ modulo01/referencias.php
@@ -17,6 +17,65 @@ include('layout/header.php');
     ?>
     <!-- FIM DO MENU  -->
 
+    <!-- CONTEUDO INICIO -->
+    <div class="all-conteudo">
+        <section>
+            <div class="main-content container-fluid">
+                <div class="row">
+                    <!-- Begin row -->
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 col-sm-8 container-fluid">
+
+                        <div class="page-title">
+
+                            <h2 class="Titulo">REFERÊNCIAS</h2>
+
+                            <br>
+
+                            <p class="Texto"> <i class="bi bi-journals"></i>ASSOCIAÇÃO BRASILEIRA DE NORMAS TÉCNICAS. NBR 9241-11:
+                                <strong>Requisitos ergonômicos para trabalho de escritórios com
+                                    computadores.</strong> Parte 11-Orientações sobre usabilidade. 2002
+                            </p>
+
+                            <p class="Texto"> <i class="bi bi-journals"></i><strong> ISO/IEC 25000 </strong>Software and system engineering–Software product
+                                Quality Requirements and Evaluation (SQuaRE) - Guide to SQuaRE.
+                                International Organization for Standarization, 2014.</p>
+
+                            <p class="Texto"> <i class="bi bi-journals"></i><strong>ISO. ISO/IEC 25010:</strong> 2011 (2011). Systems and software engineering--
+                                Systems and software Quality Requirements and Evaluation (SQuaRE)--
+                                System and software quality models. Geneva, 2011. </p>
+                            <p class="Texto"> <i class="bi bi-journals"></i><strong>ABREU, L. M. Usabilidade de telefones celulares com base em
+                                    critérios ergonômicos.</strong> Tese de Mestrado pelo Programa de PósGraduação em Design do Departamento de Artes & Design, PUC-Rio.
+                                Rio de Janeiro. 2005. </p>
+                            <p class="Texto"> <i class="bi bi-journals"></i>REBELO, Irla B.<strong>Usabilidade e suas metas.</strong> Apostila. Brasília, DF. Última
+                                atualização em novembro de 2009. Disponível em
+                                <irlabr.wordpresss.com>.
+                            </p>
+                            <p class="Texto"> <i class="bi bi-journals"></i>NIELSEN, J. (2012, January 3). <strong>Usability 101: Introduction to Usability.</strong>
+                                Acessado em novembro de 2020. Disponível em
+                                < https: //www.nngroup.com/articles/usability-101-introduction-tousability />.
+                            </p>
+                            <p class="Texto"> <i class="bi bi-journals"></i>BARBOSA, Simone; SILVA, Bruno. <strong>Interação humano-computador.</strong>
+                                Elsevier Brasil, 2010. </p>
+                            <p class="Texto"> <i class="bi bi-journals"></i>NIELSEN, J. (1994, April 24). <strong> Usability Heuristics for User Interface
+                                    Design.</strong> Retrieved November 16, 2020, from https://www.nngroup.com/
+                                articles/ten-usability-heuristics/ </p>
+
+
+
+                            <!-- Botões de navegação -->
+                            <div class="text-center">
+                                <a href="topico04.php" class="btn btn-outline-success" role="button" aria-pressed="true">
+                                    <i class="fa-solid fa-chevron-left"></i> Voltar </a>
+                                <a href="topico01.html" class="btn btn-outline-success" role="button" aria-pressed="true">Realizar a Atividade Avaliativa <i class="fa-solid fa-chevron-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
     <!-- COMEÇO DO FOOTER  -->
     <?php
     include('layout/footer.php');
--- modulo01/topico03.php
+++ modulo01/topico03.php
@@ -11,15 +11,58 @@ include('layout/header.php');
     <!-- FIM DA NAV -->
 
 
-    <!-- menu lateral começa aqui -->
+    <!-- MENU LATERAL COMEÇO -->
     <?php
     include('layout/menu.php');
     ?>
+    <!-- FIM MENU LATERAL  -->
 
+    <!-- CONTEUDO INICIO -->
+    <div class="all-conteudo">
+        <section class="conteiner-principal">
+            <div class="main-content container-fluid">
+                <div class="row">
+                    <!-- Begin row -->
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 col-sm-8 container-fluid">
 
-<?php
-include('layout/footer.php');
-?>
+                        <!--  Título Pricipal -->
+                        <h2 class="Titulo fw-bolder">03 - Heurísticas de Nielsen</h2>
+
+                        <!-- paragrafo com scroll reveal  -->
+                        <p class="scrool">Como já foi dito anteriormente, Nielsen consolidou um conjunto de 10
+                            heurísticas de usabilidade, que são amplamente utilizadas hoje [8]. No
+                            subtópicos a seguir veremos em detalhes cada heurística de forma aplicada a
+                            interfaces reais. A lista abaixo apresenta o título em português e em inglês
+                            das heurísticas:
+                        </p>
+
+                        <ol>
+                            <li>Visibilidade de qual estado estamos no sistema (Visibility of system
+                                status)</li>
+                            <li> Correspondência entre o sistema e o mundo real (Match between
+                                system and the real world)</li>
+                            <li>Liberdade de controle fácil pro usuário (User control and freedom)</li>
+                            <li>Consistência e padrões (Consistency and standards)</li>
+                            <li>Prevenção de erros (Error prevention)</li>
+                            <li>Reconhecimento em vez de memorização (Recognition rather than
+                                recall)</li>
+                            <li>Flexibilidade e eficiência de uso (Flexibility and efficiency of use)</li>
+                            <li>Estética e design minimalista (Aesthetic and minimalist design)</li>
+                            <li>Ajude os usuários a reconhecerem, diagnosticarem e recuperarem-se de
+                                erros (Help users recognize, diagnose, and recover from errors)</li>
+                            <li>Ajuda e documentação (Help and documentation)</li>
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <?php
+    include('layout/footer.php');
+    ?>
 </body>
 
 </html>
