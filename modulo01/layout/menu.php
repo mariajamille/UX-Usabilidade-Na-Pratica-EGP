1673b622085f42544e5379319767c60bf3081d1c
Maria Jamille <maria.jamille1984@gmail.com>
2023-10-24 16:51:32 -0300

    muitas coisas

--- modulo01/layout/menu.php
+++ modulo01/layout/menu.php
@@ -33,15 +33,15 @@
         <!-- subtopicos aqui  -->
         <div class="dropdown-container">
             <a href="topico02.php">
-                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
+                <i class="bi bi-journals circulo"></i>Início</a>
             <a href="#item2.1">
-                <i class="fa-regular fa-pen-to-square circulo"></i>2.1 - NBR ISO 9241-11</a>
+                <i class="bi bi-journals circulo"></i>2.1 - NBR ISO 9241-11</a>
             <a href="#item2.2">
-                <i class="fa-regular fa-pen-to-square circulo"></i>2.2 -ISO/IEC 25000 (SQuaRE)</a>
+                <i class="bi bi-journals circulo"></i>2.2 -ISO/IEC 25000 (SQuaRE)</a>
             <a href="#item2.3">
-                <i class="fa-regular fa-pen-to-square circulo"></i>2.3 - Heurísticas de Nielsen</a>
+                <i class="bi bi-journals circulo"></i>2.3 - Heurísticas de Nielsen</a>
             <a href="#item2.4">
-                <i class="fa-regular fa-pen-to-square circulo"></i>2.4 - O que não é usual?</a>
+                <i class="bi bi-journals circulo"></i>2.4 - O que não é usual?</a>
         </div>
 
         <!-- topico com Subtópico -->
@@ -57,28 +57,28 @@
         <!-- subtopicos aqui  -->
         <div class="dropdown-container">
             <a href="topico03.php">
-                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
+                <i class="bi bi-journals circulo"></i>Início</a>
             <a href="#item3.1">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.1 - Visibilidade de qual estado estamos no sistema</a>
+                <i class="bi bi-journals circulo"></i>3.1 - Visibilidade de qual estado estamos no sistema</a>
             <a href="#item3.2">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.2 - Correspondência entre o sistema e o mundo real</a>
+                <i class="bi bi-journals circulo"></i>3.2 - Correspondência entre o sistema e o mundo real</a>
             <a href="#item3.3">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.3 - Liberdade de controle fácil pro usuário</a>
+                <i class="bi bi-journals circulo"></i>3.3 - Liberdade de controle fácil pro usuário</a>
             <a href="#item3.4">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.4 - Consistência e Padrões</a>
+                <i class="bi bi-journals circulo"></i>3.4 - Consistência e Padrões</a>
             <a href="#item3.5">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.5 - Prevenção de Erros</a>
+                <i class="bi bi-journals circulo"></i>3.5 - Prevenção de Erros</a>
             <a href="#item3.6">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.6 - Reconhecimento em vez de memorização</a>
+                <i class="bi bi-journals circulo"></i>3.6 - Reconhecimento em vez de memorização</a>
             <a href="#item3.7">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.7 - Flexibilidade e Eficiência de Uso</a>
+                <i class="bi bi-journals circulo"></i>3.7 - Flexibilidade e Eficiência de Uso</a>
             <a href="#item3.8">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.8 - Estética e design minimalista</a>
+                <i class="bi bi-journals circulo"></i>3.8 - Estética e design minimalista</a>
             <a href="#item3.9">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.9 - Ajude os Usuários a Reconhecerem, Diagnosticarem e
+                <i class="bi bi-journals circulo"></i>3.9 - Ajude os Usuários a Reconhecerem, Diagnosticarem e
                 Recuperarem-se de Erros</a>
             <a href="#item3.10">
-                <i class="fa-regular fa-pen-to-square circulo"></i>3.10 - Ajuda e Documentação</a>
+                <i class="bi bi-journals circulo"></i>3.10 - Ajuda e Documentação</a>
         </div>
 
         <!-- topico com Subtópico -->
@@ -94,11 +94,11 @@
         <!-- subtopicos aqui  -->
         <div class="dropdown-container">
             <a href="topico04.php">
-                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
+                <i class="bi bi-journals circulo"></i>Início</a>
             <a href="#item4.1">
-                <i class="fa-regular fa-pen-to-square circulo"></i>4.1 - Por que avaliar?</a>
+                <i class="bi bi-journals circulo"></i>4.1 - Por que avaliar?</a>
             <a href="#item4.2">
-                <i class="fa-regular fa-pen-to-square circulo"></i>4.2 - Como Avaliar</a>
+                <i class="bi bi-journals circulo"></i>4.2 - Como Avaliar</a>
         </div>
 
         <li class="menu">
--- modulo01/referencias.php
+++ modulo01/referencias.php
@@ -34,32 +34,32 @@ include('layout/header.php');
 
                             <br>
 
-                            <p class="Texto"> <i class="bi bi-journals"></i>ASSOCIAÇÃO BRASILEIRA DE NORMAS TÉCNICAS. NBR 9241-11:
+                            <p class="Texto"> <i class="bi bi-book "></i> ASSOCIAÇÃO BRASILEIRA DE NORMAS TÉCNICAS. NBR 9241-11:
                                 <strong>Requisitos ergonômicos para trabalho de escritórios com
                                     computadores.</strong> Parte 11-Orientações sobre usabilidade. 2002
                             </p>
 
-                            <p class="Texto"> <i class="bi bi-journals"></i><strong> ISO/IEC 25000 </strong>Software and system engineering–Software product
+                            <p class="Texto"> <i class="bi bi-book"></i><strong> ISO/IEC 25000 </strong>Software and system engineering–Software product
                                 Quality Requirements and Evaluation (SQuaRE) - Guide to SQuaRE.
                                 International Organization for Standarization, 2014.</p>
 
-                            <p class="Texto"> <i class="bi bi-journals"></i><strong>ISO. ISO/IEC 25010:</strong> 2011 (2011). Systems and software engineering--
+                            <p class="Texto"> <i class="bi bi-book"></i><strong> ISO. ISO/IEC 25010:</strong> 2011 (2011). Systems and software engineering--
                                 Systems and software Quality Requirements and Evaluation (SQuaRE)--
                                 System and software quality models. Geneva, 2011. </p>
-                            <p class="Texto"> <i class="bi bi-journals"></i><strong>ABREU, L. M. Usabilidade de telefones celulares com base em
+                            <p class="Texto"> <i class="bi bi-book"></i><strong> ABREU, L. M. Usabilidade de telefones celulares com base em
                                     critérios ergonômicos.</strong> Tese de Mestrado pelo Programa de PósGraduação em Design do Departamento de Artes & Design, PUC-Rio.
                                 Rio de Janeiro. 2005. </p>
-                            <p class="Texto"> <i class="bi bi-journals"></i>REBELO, Irla B.<strong>Usabilidade e suas metas.</strong> Apostila. Brasília, DF. Última
+                            <p class="Texto"> <i class="bi bi-book"></i> REBELO, Irla B.<strong>Usabilidade e suas metas.</strong> Apostila. Brasília, DF. Última
                                 atualização em novembro de 2009. Disponível em
                                 <a href="irlabr.wordpresss.com">irlabr.wordpresss.com</a>.
                             </p>
-                            <p class="Texto"> <i class="bi bi-journals"></i>NIELSEN, J. (2012, January 3). <strong>Usability 101: Introduction to Usability.</strong>
+                            <p class="Texto"> <i class="bi bi-book"></i> NIELSEN, J. (2012, January 3). <strong>Usability 101: Introduction to Usability.</strong>
                                 Acessado em novembro de 2020. Disponível em
                                 <a href="https: //www.nngroup.com/articles/usability-101-introduction-tousability"> https: //www.nngroup.com/articles/usability-101-introduction-tousability</a>
                             </p>
-                            <p class="Texto"> <i class="bi bi-journals"></i>BARBOSA, Simone; SILVA, Bruno. <strong>Interação humano-computador.</strong>
+                            <p class="Texto"> <i class="bi bi-book"></i> BARBOSA, Simone; SILVA, Bruno. <strong>Interação humano-computador.</strong>
                                 Elsevier Brasil, 2010. </p>
-                            <p class="Texto"> <i class="bi bi-journals"></i>NIELSEN, J. (1994, April 24). <strong> Usability Heuristics for User Interface
+                            <p class="Texto"> <i class="bi bi-book"></i>NIELSEN, J. (1994, April 24). <strong> Usability Heuristics for User Interface
                                     Design.</strong> Retrieved November 16, 2020, from <a href="https://www.nngroup.com/"> https://www.nngroup.com/
                                 articles/ten-usability-heuristics/</a> </p>
 
